9d051fa2c896c9f195ea879b34edb726a6da5762
Lim Ding Wen <limdingwen@gmail.com>
2022-09-29 17:08:31 +0800

    Add skeleton

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,9 +1,28 @@
+const CommandHub = require("./CommandHub.js");
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.cmd = functions.region("asia-southeast1").https.onRequest((req, res) => {
+	//functions.logger.info("Hello logs!", {structuredData: true});
+	//response.send("Hello from Firebase!");
+	
+	let cmdData;
+	try {
+		cmdData = JSON.parse(req.rawBody);
+	}
+	catch (e) {
+		if (e instanceof SyntaxError) {
+			cmdData = {};
+		}
+		else {
+			throw e;
+		}
+	}
+	
+	let commandHub = new CommandHub();
+	let cmd = commandHub.DecodeCommand(cmdData.cmd);
+
+	cmd.ExecuteCommand(cmdData, res);
+});
