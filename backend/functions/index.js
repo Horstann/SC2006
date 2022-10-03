9d5398c4914aea7f07c1ecff5552b56cd9f57a20
Lim Ding Wen <limdingwen@gmail.com>
2022-10-03 21:00:28 +0800

    Add logging

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -23,6 +23,8 @@ exports.cmd = functions.region("asia-southeast1").https.onRequest(async (req, re
 		}
 	}
 
+	functions.logger.log("New request: ", req.rawBody.toString());
+
 	global.authoriser = new Authoriser();
 	await global.authoriser.CheckAuth(cmdData, res);
 });
