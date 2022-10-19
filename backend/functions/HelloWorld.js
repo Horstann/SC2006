f4481679f5884e35041c18c456899268373d6ad3
Lim Ding Wen <limdingwen@gmail.com>
2022-10-19 10:28:14 +0800

    Add helloworld account debugging

--- backend/functions/HelloWorld.js
+++ backend/functions/HelloWorld.js
@@ -1,9 +1,17 @@
 class HelloWorld {
 	ExecuteCommand(cmdData, acc, res) {
-		res.json({
-			"status": 0,
-			"helloWorld": "Hello world from Ignis!"
-		});
+		if (acc == null) {
+			res.json({
+				"status": 0,
+				"helloWorld": "Hello world from an anonymous account!"
+			});
+		}
+		else {
+			res.json({
+				"status": 0,
+				"helloWorld": "Hello world from account with home address: " + acc.data().HomeAddress + "!"
+			});
+		}
 	}
 }
 
