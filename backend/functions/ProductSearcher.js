6567c91fb38fcb896937af67b83d2dc225863408
Lim Ding Wen <limdingwen@gmail.com>
2022-10-31 12:28:20 +0800

    Finish searchProducts

--- backend/functions/ProductSearcher.js
+++ backend/functions/ProductSearcher.js
@@ -1,13 +1,23 @@
 const { getFirestore, Timestamp } = require('firebase-admin/firestore');
+const functions = require("firebase-functions");
 
 // As query is too complex, it's unable to be pagenated.
 // So function returns the full array of relevant products
 
 class ProductSearcher {
 	async ExecuteCommand(cmdData, acc, res) {
+		if (cmdData.searchTerm == undefined) {
+			res.json({"status": 9});
+			return;
+		}
+
+		if (acc == null) {
+			res.json({"status": 6});
+			return;
+		}
+
 		let buyerLat = acc.data().HomeLocation.latitude;
 		let buyerLong = acc.data().HomeLocation.longtitude;
-		let searchTerm = cmdData.searchTerm.trim().toLowerCase();
 
 		const db = getFirestore();
 		const productRefs = db.collection('Product');
@@ -24,9 +34,10 @@ class ProductSearcher {
 		*/
 
 		let search_res = [];
-		snapshot.forEach(async doc => {
-			if (doc.data().Name.trim().toLowerCase().includes(searchTerm) && doc.data().ClosingTime <= Timestamp.now()) {
-				
+		for (const doc of snapshot.docs) {
+			if ((doc.data().Name.toLowerCase().includes(cmdData.searchTerm.toLowerCase())
+			|| doc.data().Description.toLowerCase().includes(cmdData.searchTerm.toLowerCase()))
+			&& doc.data().ClosingTime.toMillis() > Timestamp.now().toMillis()) {
 				const sellerId = doc.data().Seller.id;
 				const sellerRef = db.collection("User").doc(sellerId);
 				const sellerDoc = await sellerRef.get();
@@ -38,6 +49,8 @@ class ProductSearcher {
 				let date1 = [date.getDate().toString().padStart(2,'0'),(date.getMonth() + 1).toString().padStart(2,'0'),date.getFullYear(),].join('/')
 				let date2 = date.getHours().toString().padStart(2,'0') + ":" + date.getMinutes().toString().padStart(2,'0')
 				let dateString = date1 + " " + date2;
+
+				functions.logger.log("Distance in KM: " + distanceInKm);
 
 				search_res.push([{
 					"productId": doc.id,
@@ -52,7 +65,7 @@ class ProductSearcher {
 					"distanceFromUser": distanceInKm
 				}, distanceInKm]);
 			}
-		});
+		}
 
 		if (search_res.length == 0) res.json({"status": 7});
 		search_res.sort(function(a, b){return a[1]-b[1]});
@@ -64,8 +77,7 @@ class ProductSearcher {
 
 		res.json({
 			"status": 0,
-			"products": search_res,
-			"acc": acc
+			"products": search_res
 		});
 	}
 }
