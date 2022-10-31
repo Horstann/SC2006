8bb6f0ec68ba7cb76e13a1a31271a90657357917
Lim Ding Wen <limdingwen@gmail.com>
2022-10-31 17:30:01 +0800

    Finish editProduct

--- backend/functions/ProductEditor.js
+++ backend/functions/ProductEditor.js
@@ -1,17 +1,32 @@
 const { getFirestore } = require('firebase-admin/firestore');
+const ImageUploader = require("./ImageUploader.js");
 
 class ProductEditor {
 	async ExecuteCommand(cmdData, acc, res) {
+        if (cmdData.productId == undefined) {
+            res.json({"status": 9});
+            return;
+        }
+
 		const db = getFirestore();
         const productRef = db.collection("Product").doc(cmdData.productId);
         const doc = await productRef.get();
-		if (!doc.exists) res.json({"status": 7});
 
-        if (cmdData.name != null) await productRef.update({Name: cmdData.name});
-        if (cmdData.totalUnits != null) await productRef.update({TotalUnits: cmdData.totalUnits});
-        if (cmdData.priceThresholds != null) await productRef.update({PriceThresholds: cmdData.priceThresholds});
-        if (cmdData.unitThresholds != null) await productRef.update({UnitThresholds: cmdData.unitThresholds});
-        if (cmdData.closingTime != null){
+		if (!doc.exists) {
+            res.json({"status": 7});
+            return;
+        }
+
+        if (doc.data().Seller.id != acc.id) {
+            res.json({"status": 6});
+            return;
+        }
+
+        if (cmdData.name != undefined) await productRef.update({Name: cmdData.name});
+        if (cmdData.totalUnits != undefined) await productRef.update({TotalUnits: cmdData.totalUnits});
+        if (cmdData.priceThresholds != undefined) await productRef.update({PriceThresholds: cmdData.priceThresholds});
+        if (cmdData.unitThresholds != undefined) await productRef.update({UnitThresholds: cmdData.unitThresholds});
+        if (cmdData.closingTime != undefined){
             const [dateValues, timeValues] = (cmdData.closingTime+":00").split(' ');
             const [month, day, year] = dateValues.split('/');
             const [hours, minutes, seconds] = timeValues.split(':');
@@ -20,13 +35,13 @@ class ProductEditor {
 
             await productRef.update({ClosingTime: timestamp});
         }
-        if (cmdData.desc != null) await productRef.update({Desc: cmdData.desc});
-        if (cmdData.pics != null) await productRef.update({Pictures: cmdData.pics});
+        if (cmdData.desc != undefined) await productRef.update({Description: cmdData.desc});
+        if (cmdData.pics != undefined) {
+            await productRef.update({Pictures: cmdData.pics});
+        }
 
 		res.json({
 			"status": 0,
-            "pictures": productRef.data().Pictures,
-			"productId": cmdData.productId
 		});
 	}
 }
