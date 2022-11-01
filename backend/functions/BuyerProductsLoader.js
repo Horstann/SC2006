b45d4bfc331bd54f1cfad798b043a32a3f15d84f
Lim Ding Wen <limdingwen@gmail.com>
2022-11-02 00:26:52 +0800

    Finish buyer products loader

--- backend/functions/BuyerProductsLoader.js
+++ backend/functions/BuyerProductsLoader.js
@@ -12,50 +12,56 @@ class BuyerProductsLoader {
 		if (acc.data().BoughtProducts) res.json({"status": 7});
 
 		let products = [];
-		snapshot.forEach(async doc => {
-			if (doc.id in acc.data().BoughtProducts) {
-				let unitsBought = acc.data().BoughtProducts[doc.id];
+		for (const doc of snapshot.docs) {
+			for (const boughtProduct of acc.data().BoughtProducts) { // yes this is unoptimised af, sue me
+				if (doc.id == boughtProduct.id) {
+					let unitsBought = acc.data().BoughtProducts[doc.id];
 
-				const sellerId = doc.data().Seller.id;
-				const sellerRef = db.collection("User").doc(sellerId);
-				const sellerDoc = await sellerRef.get();
-				let sellerLat = sellerDoc.data().HomeLocation.latitude;
-				let sellerLong = sellerDoc.data().HomeLocation.longitude;
-				let distanceInKm = Math.sqrt(((buyerLat-sellerLat)*110.547)**2 + (111.320*Math.cos(buyerLong-sellerLong))**2);
-				
-				let isClosed = false;
-				if (doc.data().ClosingTime < Timestamp.now()) isClosed = true;
+					const sellerId = doc.data().Seller.id;
+					const sellerRef = db.collection("User").doc(sellerId);
+					const sellerDoc = await sellerRef.get();
+					let sellerLat = sellerDoc.data().HomeLocation.latitude;
+					let sellerLong = sellerDoc.data().HomeLocation.longitude;
+					let distanceInKm = Math.sqrt(((buyerLat-sellerLat)*110.547)**2 + (111.320*Math.cos(buyerLong-sellerLong))**2);
+					
+					let isClosed = false;
+					if (doc.data().ClosingTime < Timestamp.now()) isClosed = true;
 
-				let timestamp = doc.data().ClosingTime;
-				let date = timestamp.toDate();
-				let date1 = [date.getDate().toString().padStart(2,'0'),(date.getMonth() + 1).toString().padStart(2,'0'),date.getFullYear(),].join('/')
-				let date2 = date.getHours().toString().padStart(2,'0') + ":" + date.getMinutes().toString().padStart(2,'0')
-				let dateString = date1 + " " + date2;
+					let timestamp = doc.data().ClosingTime;
+					let date = timestamp.toDate();
+					let date1 = [date.getDate().toString().padStart(2,'0'),(date.getMonth() + 1).toString().padStart(2,'0'),date.getFullYear(),].join('/')
+					let date2 = date.getHours().toString().padStart(2,'0') + ":" + date.getMinutes().toString().padStart(2,'0')
+					let dateString = date1 + " " + date2;
 
-				search_res.push([{
-					"productId": doc.id,
-					"name": doc.data().Name,
-					"unitsBought": unitsBought,
-					"totalUnits": doc.data().TotalUnits,
-					"totalBought": doc.data().TotalBought,
-					"priceThresholds": doc.data().PriceThresholds,
-					"unitThresholds": doc.data().UnitThresholds,
-					"closingTime": dateString,
-					"desc": doc.data().Description,
-					"pics": doc.data().Pictures,
-					"distanceFromUser": distanceInKm,
-					"isClosed": isClosed
-				}, distanceInKm]);
+					products.push([{
+						"productId": doc.id,
+						"name": doc.data().Name,
+						"unitsBought": unitsBought,
+						"totalUnits": doc.data().TotalUnits,
+						"totalBought": doc.data().TotalBought,
+						"priceThresholds": doc.data().PriceThresholds,
+						"unitThresholds": doc.data().UnitThresholds,
+						"closingTime": dateString,
+						"desc": doc.data().Description,
+						"pics": doc.data().Pictures,
+						"distanceFromUser": distanceInKm,
+						"isClosed": isClosed
+					}, distanceInKm]);
+				}
 			}
-		});
+		}
 
-		if (search_res.length == 0) res.json({"status": 7});
-		search_res.sort(function(a, b){return a[1]-b[1]});
+		if (products.length == 0) {
+			res.json({"status": 7});
+			return;
+		}
+
+		products.sort(function(a, b){return a[1]-b[1]});
 
 		function funct(item, index, arr) {
 			arr[index] = item[0];
 		}
-		search_res.forEach(funct);
+		products.forEach(funct);
 
 		res.json({
 			"status": 0,
--- backend/functions/SellerProductsLoader.js
+++ backend/functions/SellerProductsLoader.js
@@ -2,37 +2,44 @@ const { getFirestore } = require('firebase-admin/firestore');
 
 class SellerProductsLoader {
 	async ExecuteCommand(cmdData, acc, res) {
+		if (acc == null) {
+			res.json({"status": 6});
+			return;
+		}
 
 		const db = getFirestore();
 		const productRefs = db.collection('Product');
 		const snapshot = await productRefs.get();
 
 		let products = [];
-		snapshot.forEach(async doc => {
-            const sellerId = doc.data().Seller.id;
+		for (const doc of snapshot.docs) {
+			const sellerId = doc.data().Seller.id;
 
-            if (sellerId = acc.id){
+			if (sellerId == acc.id){
 				let timestamp = doc.data().ClosingTime;
 				let date = timestamp.toDate();
 				let date1 = [date.getDate().toString().padStart(2,'0'),(date.getMonth() + 1).toString().padStart(2,'0'),date.getFullYear(),].join('/')
 				let date2 = date.getHours().toString().padStart(2,'0') + ":" + date.getMinutes().toString().padStart(2,'0')
 				let dateString = date1 + " " + date2;
 
-                products.push({
-                    "productId": doc.id,
-                    "name": doc.data().Name,
-                    "totalUnits": doc.data().TotalUnits,
-                    "totalBought": doc.data().TotalBought,
-                    "priceThresholds": doc.data().PriceThresholds,
-                    "unitThresholds": doc.data().UnitThresholds,
-                    "closingTime": dateString,
-                    "desc": doc.data().Description,
-                    "pics": doc.data().Pictures,
-                });
+				products.push({
+						"productId": doc.id,
+						"name": doc.data().Name,
+						"totalUnits": doc.data().TotalUnits,
+						"totalBought": doc.data().TotalBought,
+						"priceThresholds": doc.data().PriceThresholds,
+						"unitThresholds": doc.data().UnitThresholds,
+						"closingTime": dateString,
+						"desc": doc.data().Description,
+						"pics": doc.data().Pictures,
+				});
 			}
-		});
+		}
 
-		if (products.length == 0) res.json({"status": 7});
+		if (products.length == 0) {
+			res.json({"status": 7});
+			return;
+		}
 
 		res.json({
 			"status": 0,
