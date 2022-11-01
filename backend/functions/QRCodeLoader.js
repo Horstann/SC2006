716b3fa7d7de01f329588a4ca32affcf8c4e3c88
Lim Ding Wen <limdingwen@gmail.com>
2022-11-01 12:25:56 +0800

    Finish load qr code

--- backend/functions/QRCodeLoader.js
+++ backend/functions/QRCodeLoader.js
@@ -2,18 +2,28 @@ const { getFirestore } = require('firebase-admin/firestore');
 
 class QRCodeLoader {
 	async ExecuteCommand(cmdData, acc, res) {
+		if (cmdData.productId == undefined) {
+			res.json({"status": 9});
+			return;
+		}
+
 		const db = getFirestore();
 		const productRef = db.collection("Product").doc(cmdData.productId);
 		const doc = await productRef.get();
-		if (!doc.exists) res.json({"status": 7});
+		if (!doc.exists) {
+			res.json({"status": 7});
+			return;
+		}
 
 		const sellerId = doc.data().Seller.id;
 		const sellerRef = db.collection("User").doc(sellerId);
 		const sellerDoc = await sellerRef.get();
+		if (sellerDoc.data().QRCode == "") {
+			res.json({"status": 10});
+			return;
+		}
 
-        if (sellerDoc.data().QRCode == null) res.json({"status": 10});
-
-        res.json({
+		res.json({
 			"status": 0,
 			"qrcode": sellerDoc.data().QRCode
 		});
