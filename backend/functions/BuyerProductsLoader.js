7de71296c36043997e6b9e2ef4f22c6dfe39fb32
Lim Ding Wen <limdingwen@gmail.com>
2022-11-02 11:50:52 +0800

    Fix buyerProductsLoader to use maps

--- backend/functions/BuyerProductsLoader.js
+++ backend/functions/BuyerProductsLoader.js
@@ -11,41 +11,39 @@ class BuyerProductsLoader {
 
 		let products = [];
 		for (const doc of snapshot.docs) {
-			for (const boughtProduct of acc.data().BoughtProducts) { // yes this is unoptimised af, sue me
-				if (doc.id == boughtProduct.id) {
-					let unitsBought = acc.data().BoughtProducts[doc.id];
+			if (doc.id in acc.data().BoughtProducts) { // yes this is unoptimised af, sue me
+				let unitsBought = acc.data().BoughtProducts[doc.id];
 
-					const sellerId = doc.data().Seller.id;
-					const sellerRef = db.collection("User").doc(sellerId);
-					const sellerDoc = await sellerRef.get();
-					let sellerLat = sellerDoc.data().HomeLocation.latitude;
-					let sellerLong = sellerDoc.data().HomeLocation.longitude;
-					let distanceInKm = Math.sqrt(((buyerLat-sellerLat)*110.547)**2 + (111.320*Math.cos(buyerLong-sellerLong))**2);
-					
-					let isClosed = false;
-					if (doc.data().ClosingTime.toMillis() < Timestamp.now().toMillis()) isClosed = true;
+				const sellerId = doc.data().Seller.id;
+				const sellerRef = db.collection("User").doc(sellerId);
+				const sellerDoc = await sellerRef.get();
+				let sellerLat = sellerDoc.data().HomeLocation.latitude;
+				let sellerLong = sellerDoc.data().HomeLocation.longitude;
+				let distanceInKm = Math.sqrt(((buyerLat-sellerLat)*110.547)**2 + (111.320*Math.cos(buyerLong-sellerLong))**2);
+				
+				let isClosed = false;
+				if (doc.data().ClosingTime.toMillis() < Timestamp.now().toMillis()) isClosed = true;
 
-					let timestamp = doc.data().ClosingTime;
-					let date = timestamp.toDate();
-					let date1 = [date.getDate().toString().padStart(2,'0'),(date.getMonth() + 1).toString().padStart(2,'0'),date.getFullYear(),].join('/')
-					let date2 = date.getHours().toString().padStart(2,'0') + ":" + date.getMinutes().toString().padStart(2,'0')
-					let dateString = date1 + " " + date2;
+				let timestamp = doc.data().ClosingTime;
+				let date = timestamp.toDate();
+				let date1 = [date.getDate().toString().padStart(2,'0'),(date.getMonth() + 1).toString().padStart(2,'0'),date.getFullYear(),].join('/')
+				let date2 = date.getHours().toString().padStart(2,'0') + ":" + date.getMinutes().toString().padStart(2,'0')
+				let dateString = date1 + " " + date2;
 
-					products.push([{
-						"productId": doc.id,
-						"name": doc.data().Name,
-						"unitsBought": unitsBought,
-						"totalUnits": doc.data().TotalUnits,
-						"totalBought": doc.data().TotalBought,
-						"priceThresholds": doc.data().PriceThresholds,
-						"unitThresholds": doc.data().UnitThresholds,
-						"closingTime": dateString,
-						"desc": doc.data().Description,
-						"pics": doc.data().Pictures,
-						"distanceFromUser": distanceInKm,
-						"isClosed": isClosed
-					}, distanceInKm]);
-				}
+				products.push([{
+					"productId": doc.id,
+					"name": doc.data().Name,
+					"unitsBought": unitsBought,
+					"totalUnits": doc.data().TotalUnits,
+					"totalBought": doc.data().TotalBought,
+					"priceThresholds": doc.data().PriceThresholds,
+					"unitThresholds": doc.data().UnitThresholds,
+					"closingTime": dateString,
+					"desc": doc.data().Description,
+					"pics": doc.data().Pictures,
+					"distanceFromUser": distanceInKm,
+					"isClosed": isClosed
+				}, distanceInKm]);
 			}
 		}
 
