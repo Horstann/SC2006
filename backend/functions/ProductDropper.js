f2900425a34d81bdd01f3a52f63db69716fe00c4
Lim Ding Wen <limdingwen@gmail.com>
2022-11-02 22:20:06 +0800

    Finish dropProduct

--- backend/functions/ProductDropper.js
+++ backend/functions/ProductDropper.js
@@ -3,46 +3,58 @@ const { admin } = require('firebase-admin');
 
 class ProductDropper {
 	async ExecuteCommand(cmdData, acc, res) {
+        if (
+            cmdData.dropAll == undefined ||
+            cmdData.productId == undefined ||
+            cmdData.quantity == undefined
+        ) {
+            res.json({"status": 9});
+            return;
+        }
+
+        if (acc == null) {
+            res.json({"status": 6});
+            return;
+        }
+
 		const db = getFirestore();
 		const productRef = db.collection("Product").doc(cmdData.productId);
 		const doc = await productRef.get();
 
-		if (!doc.exists) res.json({"status": 7});
-		if (doc.data().closingTime > Timestamp.now()) res.json({"status": 11});
-        if (!(doc.id in acc.BoughtProducts)) res.json({"status": 14});
-
-        let newBoughtProducts = doc.data().BoughtProducts;
-        if (doc.data().dropAll != null){
-            if (doc.data().dropAll == true) {
-                delete newBoughtProducts[doc.id];
-                await acc.set({
-                    BoughtProducts: newBoughtProducts
-                });
-            }
+		if (!doc.exists) {
+            res.json({"status": 7});
+            return;
         }
-        if (cmdData.quantity > (acc.BoughtProducts[doc.id]) || cmdData.quantity < 1) res.json({"status": 13});
-
-		let newTotalBought = doc.data().TotalBought - cmdData.quantity;
-		await doc.set({
-				TotalBought: newTotalBought
-		});
-
-        let newQuantity = acc.BoughtProducts[doc.id] - cmdData.quantity;
-
-        if (newQuantity == 0){
-            delete newBoughtProducts[doc.id];
-            await acc.set({
-                BoughtProducts: newBoughtProducts
-            });
+        
+		if (doc.data().ClosingTime.toMillis() < Timestamp.now().toMillis()) {
+            res.json({"status": 11});
+            return;
         }
-        else{
-            newBoughtProducts[doc.id] = newQuantity;
-            await acc.set({
-                BoughtProducts: newBoughtProducts
-            });
-        }        
-		
-		res.json({"status": 7});
+
+        let boughtProducts = acc.data().BoughtProducts;
+        if (boughtProducts[doc.id] == undefined) {
+            boughtProducts[doc.id] = 0;
+        }
+
+        if (cmdData.dropAll) {
+            cmdData.quantity = boughtProducts[doc.id];
+        }
+
+        if (boughtProducts[doc.id] < cmdData.quantity) {
+            res.json({"status": 13});
+            return;
+        }
+
+        boughtProducts[doc.id] -= cmdData.quantity;
+        acc.ref.update({
+            BoughtProducts: boughtProducts
+        });
+
+        productRef.update({
+            TotalBought: doc.data().TotalBought - cmdData.quantity
+        });
+
+		res.json({"status": 0});
 	}
 }
 
